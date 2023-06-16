f47293c60081cc25a4344025f6314a969439fa78
kirubaFlycart <kirubanithi@flycart.org>
2023-06-16 13:36:05 +0530

    - Message translate added

--- app/Controllers/Admin/Admin.php
+++ app/Controllers/Admin/Admin.php
@@ -40,6 +40,7 @@ class Admin
         if (!empty($min_price)) {
             $settings_data = get_option('wdr_omnibus_directive');
             $message = isset($settings_data['message']) && !empty($settings_data['message']) ? $settings_data['message'] : "Preview lowest price was {{price}} updated on {{date}}";
+            $message = __($message, 'wdr-omnibus-directive');
             $message = str_replace('{{price}}', wc_price($min_price), $message);
             $date_format = apply_filters('wdr_omnibus_directive_message_date_format_for_omnibus',date_i18n(get_option('date_format'),$lowest_price_date), $lowest_price_date, $min_price);
             $message = str_replace('{{date}}', $date_format, $message);
@@ -100,6 +101,7 @@ class Admin
         }
         $settings_data = get_option('wdr_omnibus_directive');
         $message = isset($settings_data['message']) && !empty($settings_data['message']) ? $settings_data['message'] : "Preview lowest price was {{price}} updated on {{date}}";
+        $message = __($message, 'wdr-omnibus-directive');
         $message = str_replace('{{price}}', '{price}', $message);
         $message = str_replace('{{date}}', date_i18n(get_option('date_format'),"{timestamp}"), $message);
 
@@ -175,7 +177,8 @@ class Admin
         $lowest_price_date = isset($date) && !empty($date)? $date : time();
         if (!empty($min_price)) {
             $settings_data = get_option('wdr_omnibus_directive');
-            $message = isset($settings_data['message']) && !empty($settings_data['message']) ? $settings_data['message'] : "Preview lowest price was {{price}} updated from {{date}}";
+            $message = isset($settings_data['message']) && !empty($settings_data['message']) ? $settings_data['message'] : "Preview lowest price was {{price}} updated on {{date}}";
+            $message = __($message, 'wdr-omnibus-directive');
             $message = str_replace('{{price}}', wc_price($min_price), $message);
             $date_format = apply_filters('wdr_omnibus_directive_message_date_format',date_i18n(get_option('date_format'),$lowest_price_date), $lowest_price_date, $min_price);
             $message = str_replace('{{date}}', $date_format, $message);
@@ -227,6 +230,7 @@ class Admin
         if (!empty($min_price)) {
             $settings_data = get_option('wdr_omnibus_directive');
             $message = isset($settings_data['message']) && !empty($settings_data['message']) ? $settings_data['message'] : "Preview lowest price was {{price}} updated on {{date}}";
+            $message = __($message, 'wdr-omnibus-directive');
             $message = str_replace('{{price}}', wc_price($min_price), $message);
             $date_format = apply_filters('wdr_omnibus_directive_message_date_format',date_i18n(get_option('date_format'),$lowest_price_date), $lowest_price_date, $min_price);
             $message = str_replace('{{date}}', $date_format, $message);
